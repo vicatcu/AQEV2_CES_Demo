b1abcd84d713c19e7a2b50d9b6f0401332fe9582
Victor Aprea <victor.aprea@wickeddevice.com>
2014-12-23 10:04:26 -0500

    added splash screen

--- LCD.ino
+++ LCD.ino
@@ -7,4 +7,16 @@ void initializeLCD(){
   digitalWrite(A6, HIGH);
   
   lcd.begin(16, 2);
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Air Quality Egg");
+  lcd.setCursor(3, 1);
+  lcd.print("Version 2");
+  delay(3000);
+  
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("by: Wicked");
+  lcd.setCursor(7, 1);
+  lcd.print("Device");  
 }
--- Scheduler.ino
+++ Scheduler.ino
@@ -1,42 +1,43 @@
 #include <avr/interrupt.h>
-#include <util/atomic.h
-
+#include <util/atomic.h>
+ 
 typedef struct {
   uint16_t task_timer;
-  uint16_t task_period;  
+  uint16_t task_period;
   void (*task)(void);
 } task_t;
-
-////////////////////////////////////////////////////////////////////////////////////
-//                                                                                //
-// define NUM_TASKS to be at least as big as the actual number of tasks defined!1 //
-//                                                                                //
-////////////////////////////////////////////////////////////////////////////////////
-
+ 
+////////////////////////////////////////////////////////////////////
+//                                                                //
+// define NUM_TASKS to be >= the actual number of tasks defined!  //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+ 
 #define NUM_TASKS (4)
 task_t tasks[NUM_TASKS] = {0};
-
+ 
 void initializeScheduler(){
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //                                                                                                               //  
-  // statically set up the tasks, e.g.:                                                                                //  
-  // tasks[0].task_period = 500;   // execute once every 500ms (technically "no more frequantly than every 500ms") //
-  // tasks[0].task = &exampleTask; // call the function exampleTask                                                //
-  //                                                                                                               //
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  TCCR3B = _BV(WGM32) | _BV(CS31) | _BV(CS30); // prescale clock by 64 and enable CTC mode
-  OCR3A = 250;                                 // trigger compare match ever 250 ticks
-  TIMSK3 = _BV(OCIE3A);                        // enable output OCR3A compare interrupt    
+  /////////////////////////////////////////////////////////////////////////////
+  //                                                                         //
+  // statically set up the tasks here, e.g.:                                 //
+  // tasks[0].task_period = 500;   // execute once every 500ms (technically  //
+  //                               // "no more frequantly than every 500ms") //
+  // tasks[0].task = &exampleTask; // call the function exampleTask          //
+  //                                                                         //
+  /////////////////////////////////////////////////////////////////////////////
+ 
+  TCCR3B = _BV(WGM32) | _BV(CS31) | _BV(CS30); // prescaler=64, enable CTC mode
+  OCR3A = 250;                                 // compare match every 250 ticks
+  TIMSK3 = _BV(OCIE3A);                        // enable compare match ISR
 }
-
+ 
 // initializeScheduler sets this ISR up to fire once per millisecond
-ISR(TIMER3_COMPA_vect){  
+ISR(TIMER3_COMPA_vect){
   for(uint8_t ii = 0; ii < NUM_TASKS; ii++){
     if(tasks[ii].task_timer > 0) tasks[ii].task_timer--;
-  }    
+  }
 }
-
+ 
 // call this from loop()
 void executeTasks(){
   for(uint8_t ii = 0; ii < NUM_TASKS; ii++){
@@ -50,10 +51,3 @@ void executeTasks(){
     }
   }
 }
-
-// task functions must take no arguments, e.g. 
-// void exampleTask(){
-//  static uint8_t x = 0;
-//  x ^= 1;
-//  digitalWrite(A6, x);
-// }
